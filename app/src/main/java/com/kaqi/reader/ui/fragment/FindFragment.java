dbf7a1d3c8b11fa73a9a3b86520c388cedded4bc
liubing <285175775@qq.com>
2019-04-26 12:32:08 +0800

    # GitHub .gitignore

--- app/src/main/java/com/kaqi/reader/ui/fragment/FindFragment.java
+++ app/src/main/java/com/kaqi/reader/ui/fragment/FindFragment.java
@@ -15,26 +15,9 @@
  */
 package com.kaqi.reader.ui.fragment;
 
-import android.content.Intent;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-
 import com.kaqi.reader.R;
 import com.kaqi.reader.base.BaseFragment;
-import com.kaqi.reader.bean.support.FindBean;
-import com.kaqi.reader.common.OnRvItemClickListener;
 import com.kaqi.reader.component.AppComponent;
-import com.kaqi.reader.ui.activity.SubjectBookListActivity;
-import com.kaqi.reader.ui.activity.TopCategoryListActivity;
-import com.kaqi.reader.ui.activity.TopRankActivity;
-import com.kaqi.reader.ui.adapter.FindAdapter;
-import com.kaqi.reader.view.SupportDividerItemDecoration;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import butterknife.Bind;
 
 /**
  * 发现
@@ -42,13 +25,7 @@ import butterknife.Bind;
  * @author yuyh.
  * @date 16/9/1.
  */
-public class FindFragment extends BaseFragment implements OnRvItemClickListener<FindBean> {
-
-    @Bind(R.id.recyclerview)
-    RecyclerView mRecyclerView;
-
-    private FindAdapter mAdapter;
-    private List<FindBean> mList = new ArrayList<>();
+public class FindFragment extends BaseFragment {
 
     @Override
     public int getLayoutResId() {
@@ -62,20 +39,10 @@ public class FindFragment extends BaseFragment implements OnRvItemClickListener<
 
     @Override
     public void initDatas() {
-        mList.clear();
-        mList.add(new FindBean("排行榜", R.drawable.home_find_rank));
-        mList.add(new FindBean("主题书单", R.drawable.home_find_topic));
-        mList.add(new FindBean("分类", R.drawable.home_find_category));
     }
 
     @Override
     public void configViews() {
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        mRecyclerView.addItemDecoration(new SupportDividerItemDecoration(mContext, LinearLayoutManager.VERTICAL, true));
-
-        mAdapter = new FindAdapter(mContext, mList, this);
-        mRecyclerView.setAdapter(mAdapter);
 
     }
 
@@ -87,23 +54,6 @@ public class FindFragment extends BaseFragment implements OnRvItemClickListener<
     @Override
     public void attachView() {
 
-    }
-
-    @Override
-    public void onItemClick(View view, int position, FindBean data) {
-        switch (position) {
-            case 0:
-                TopRankActivity.startActivity(activity);
-                break;
-            case 1:
-                SubjectBookListActivity.startActivity(activity);
-                break;
-            case 2:
-                startActivity(new Intent(activity, TopCategoryListActivity.class));
-                break;
-            default:
-                break;
-        }
     }
 
 }
