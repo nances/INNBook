0bbfab81bccfb9028d4049e5f6abdd7e87f863f0
liubing <285175775@qq.com>
2019-04-29 11:38:53 +0800

    # 书架

--- app/src/main/java/com/kaqi/reader/ui/fragment/FindItemFragment.java
+++ app/src/main/java/com/kaqi/reader/ui/fragment/FindItemFragment.java
@@ -1,10 +1,15 @@
 package com.kaqi.reader.ui.fragment;
 
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.kaqi.reader.R;
 import com.kaqi.reader.base.BaseFragment;
 import com.kaqi.reader.component.AppComponent;
+import com.kaqi.reader.ui.adapter.FindItemAdapter;
+
+import butterknife.Bind;
 
 public class FindItemFragment extends BaseFragment {
     public static FindItemFragment newInstance(int type) {
@@ -14,6 +19,11 @@ public class FindItemFragment extends BaseFragment {
         fragment.setArguments(args);
         return fragment;
     }
+
+    @Bind(R.id.recyclerview)
+    RecyclerView mRecyclerView;
+
+    private FindItemAdapter mAdapter;
 
     @Override
     public int getLayoutResId() {
@@ -37,6 +47,16 @@ public class FindItemFragment extends BaseFragment {
 
     @Override
     public void configViews() {
+        mAdapter = new FindItemAdapter();
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 12);
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                return mAdapter.getSpanSize(position);
+            }
+        });
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
 
     }
 }
